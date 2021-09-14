754f5a9fef3864c6e4e070039475fb70cd8f7286
Fagonar96 <fagonar96.tec@gmail.com>
2021-09-14 16:23:51 -0600

    Avergage filter done

--- filter/filter.c
+++ filter/filter.c
@@ -11,79 +11,58 @@
 struct stat st = {0};
 
 
-//  method returns Nth power of A
-double nthRoot(int A, int N)
-{
-    // initially guessing a random number between
-    // 0 and 9
-    double xPre = rand() % 10;
- 
-    //  smaller eps, denotes more accuracy
-    double eps = 1e-3;
- 
-    // initializing difference between two
-    // roots by INT_MAX
-    double delX = INT_MAX;
- 
-    //  xK denotes current value of x
-    double xK;
- 
-    //  loop until we reach desired accuracy
-    while (delX > eps)
-    {
-        //  calculating current value from previous
-        // value by newton's method
-        xK = ((N - 1.0) * xPre +
-              (double)A/pow(xPre, N-1)) / (double)N;
-        delX = abs(xK - xPre);
-        xPre = xK;
-    }
- 
-    return xK;
-}
-
 /** 
- * This function applies the geometric avergae filter to an input image
+ * This function applies the filter to an input image and returns a filtered image
+ * input_image: struct with the data of the input image with noise
+ * filtered_image: struct with data of the filtered image with reduced noise
+ * window_size: size of the neighborhood window
+ *              1 -> 3 x 3 window
+ *              2 -> 5 x 5 window
+ *              3 -> 7 x 7 window
+ *              4 -> 9 x 9 window 
 */
-void filter(Image *input_image,Image *filtered_image , int window_size)
+void filter(Image *input_image, Image *filtered_image, int window_size)
 {
 
+    // Double loop to travel the frame matrix
     for (int i = 1; i < IMAGE_M - 1; i++)
     {
         for (int j = 1; j < IMAGE_N - 1; j++)
-        {
+        {   
+            // Neighboirhood boundaries
             int x_start = i - window_size;
             int x_end = i + window_size;
             int y_start = j - window_size;
             int y_end = j + window_size;
-            
-            double temp = 1;
 
+            // Initial value of filtered pixel
+            double temp = 0;
+
+            // Double loop to travel the temporary nieghborhood
             for (int x = x_start; x <= x_end; x++)
             {
                 for (int y = y_start; y <= y_end; y++)
                 {
+                    // Average filter
                     int pixel = input_image->data[x][y];
-                    //printf("Input Pixel  = %d\n", pixel);
-                    //temp = temp * pixel;
                     temp = temp + pixel;
                 }
             }
-            //printf("Output Pixel BEFORE POW= %f\n", temp);
-            //temp = round(pow(temp, 1.0/9));
-            temp = round(temp/9);
-            //printf("Output Pixel= %f\n", temp);
-
+            // Average filter
+            temp = round(temp/NEIGHBORHOOD_SIZE);
             int result = (int) temp;
-            //printf("Cast output pixel = %d\n", result);
-
             filtered_image->data[i][j] = result;
-
-            //printf("Next neighborhood\n");
         }
     }
 }
 
+
+/** 
+ * This function applies batch processing of the files containing the input 
+ * images and writes the filtered images.
+ * input_directory: directory path containing the input images.
+ * file_amount: the amount of input images to be processed.
+*/
 int process_files(const char *input_directory, int file_amount)
 {
     // Creates filtered frames folder if it doesnt exist
@@ -92,7 +71,7 @@ int process_files(const char *input_directory, int file_amount)
         mkdir("../filtered", 0700);
     }
     
-    // Reserve memory for input frames
+    // Set memory space for input frames
     Image (*input_images)[PARALLEL_FILES_TO_LOAD] = malloc(sizeof(*input_images));
     for (int i = 0; i < PARALLEL_FILES_TO_LOAD; i++)
     {
@@ -101,7 +80,7 @@ int process_files(const char *input_directory, int file_amount)
         RESET_IMAGE(  imageptr )
     }
 
-    // Reserve memory for output frames
+    // Set memory space for output frames
     Image (*filtered_images)[PARALLEL_FILES_TO_LOAD] = malloc(sizeof(*filtered_images));
     for (int i = 0; i < PARALLEL_FILES_TO_LOAD; i++)
     {
@@ -110,13 +89,13 @@ int process_files(const char *input_directory, int file_amount)
         RESET_IMAGE(imageptr)
     }
     
-    // Variable for full execution time
+    // Variable to store full execution time
     double full_time = 0;
 
     int batches = file_amount/PARALLEL_FILES_TO_LOAD;
     for (int batches_c = 0; batches_c < batches; batches_c++)
     {
-        // Load frames
+        // Load and read the batch of frames
         #pragma omp parallel for
         for (int files_c = 0; files_c < PARALLEL_FILES_TO_LOAD; files_c++)
         {
@@ -130,11 +109,11 @@ int process_files(const char *input_directory, int file_amount)
         double start_time, run_time;
         start_time = omp_get_wtime();
         
-        // Process frames
+        // Process the batch of frames
         for (int filter_c = 0; filter_c < PARALLEL_FILES_TO_LOAD; filter_c++)
         {
             
-            // Call filter
+            // Call the filter function
             filter(&(*input_images)[filter_c],&(*filtered_images)[filter_c] , 1);
             printf("Frame %d procesado.\n", filter_c);
         }
@@ -143,7 +122,7 @@ int process_files(const char *input_directory, int file_amount)
         run_time = omp_get_wtime() - start_time;
         full_time += run_time;
 
-        // Save frames
+        // Save and write the batch of frames
         #pragma omp parallel for
         for (int file_write_c = 0; file_write_c < PARALLEL_FILES_TO_LOAD; file_write_c++)
         {
@@ -157,6 +136,7 @@ int process_files(const char *input_directory, int file_amount)
 
     printf("Execution time: %f\n", full_time);
 
+    // Free up memory space
     free(filtered_images);
     free(input_images);
 
@@ -166,9 +146,6 @@ int process_files(const char *input_directory, int file_amount)
 // ./median_filter ../../frame 5
 int main(int argc, char *argv[])
 {
-    // printf("ARGC: %d\n", argc);
-
-    // TODO: validar argumentos
     int i;
     for (i = 0; i < argc; i++)
     {
@@ -177,7 +154,7 @@ int main(int argc, char *argv[])
 
     if (argc != 3)
     {
-        printf("error in arguments\n");
+        printf("Error in arguments\n");
         return 1;
     }
 
